921f79dd22da1b51102658767d2e2172b250477c
weerapon <weerapon@thinknet.co.th>
2017-04-28 00:01:19 +0700

    edit file main call function setArea in class Person

--- src/LazyClass/Main.java
+++ src/LazyClass/Main.java
@@ -16,7 +16,7 @@ public class Main {
      */
     public static void main(String[] args) {
         Person person = new Person();
-        person.getTelephone().setAreaCode("+66");
+        person.setAreaCode("+66");
     }
     
 }
